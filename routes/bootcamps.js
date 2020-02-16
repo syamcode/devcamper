e2d9752ff5a9f52fc690dada03df15b76fd93751
Muhamad Hisyam Anshory <syamcode@gmail.com>
2020-02-16 08:57:00 +0700

    Filter bootcamp by radius

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,10 +4,13 @@ const {
   getBootcamp,
   createBootcamp,
   updateBootcamp,
-  deleteBootcamp
+  deleteBootcamp,
+  getBootcampsInRadius
 } = require('../controllers/bootcamp');
 
 const router = express.Router();
+
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
 router
   .route('/')
