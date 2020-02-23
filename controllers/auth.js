b5432a868c95d95552746d8af34766e78a5046af
Muhamad Hisyam Anshory <syamcode@gmail.com>
2020-02-23 15:29:10 +0700

    Create User Model and Register function

---
+++ controllers/auth.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+const asyncHandler = require('../middleware/async');
+const ErrorResponse = require('../utils/errorResponse');
+
+// @desc        Register User
+// @route       POST /api/v1/auth/register
+// @access      Public
+exports.register = asyncHandler(async (req, res, next) => {
+  const { name, email, password, role } = req.body;
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role
+  });
+
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
