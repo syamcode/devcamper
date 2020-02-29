4847c53f3e607caaf00171a9f32de6621a02ffe2
Muhamad Hisyam Anshory <syamcode@gmail.com>
2020-02-29 21:27:25 +0700

    Logout function

--- middleware/auth.js
+++ middleware/auth.js
@@ -18,7 +18,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
   // Make sure token exists
   if (!token) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   try {
@@ -29,7 +29,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
