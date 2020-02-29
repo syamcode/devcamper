fada58831bda0e0f24be0f92527e28afccecc1ae
Muhamad Hisyam Anshory <syamcode@gmail.com>
2020-02-29 21:01:21 +0700

    Update and delete review

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -66,3 +66,62 @@ exports.addReview = asyncHandler(async (req, res, next) => {
     data: review
   });
 });
+
+// @desc        Update review
+// @route       PUT /api/v1/reviews/:id
+// @access      Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+  let review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  // Make sure user is owner of the review
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to update review ${review._id}`,
+        401
+      )
+    );
+  }
+
+  review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  res.status(200).json({ success: true, data: review });
+});
+
+// @desc        Delete review
+// @route       DELETE /api/v1/reviews/:id
+// @access      Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+  const review = await Review.findById(req.params.id);
+
+  if (!review) {
+    return next(
+      new ErrorResponse(`No review with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  // Make sure user is owner of the review
+  if (review.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(
+      new ErrorResponse(
+        `User ${req.user.id} is not authorized to delete review ${review._id}`,
+        401
+      )
+    );
+  }
+
+  review.remove();
+
+  res.status(200).json({ success: true, data: {} });
+});
